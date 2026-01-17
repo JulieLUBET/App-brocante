6aa0bd2e6239f9ae8937d8e5538711d82153b45f
JulieLUBET <julie.lubet@epita.fr>
2026-01-17 18:44:34 +0100

    corrections images

--- js/flux.js
+++ js/flux.js
@@ -5,16 +5,64 @@ document.addEventListener("DOMContentLoaded", () => {
   // Données demo (tu peux remplacer par fetch("data/flux.json"))
   const data = {
     live: [
-      { id: "p1", user: "Lucie", handle: "@Lucie", img: "../images/flux1.png", caption: "Trouvaille du jour !" },
-      { id: "p2", user: "Maeva", handle: "@Maeva445", img: "../images/flux2.png", caption: "Brocante du dimanche" },
-      { id: "p3", user: "Paul", handle: "@Paul11", img: "../images/flux3.png", caption: "Pour les amateurs de confort et de voiture ;)" },
-      { id: "p4", user: "752", handle: "@752", img: "../images/flux4.png", caption: "Service à thé complet" },
+      {
+        id: "p1",
+        user: "Lucie",
+        handle: "@Lucie",
+        img: "images/flux1.png",
+        caption: "Trouvaille du jour !",
+      },
+      {
+        id: "p2",
+        user: "Maeva",
+        handle: "@Maeva445",
+        img: "images/flux2.png",
+        caption: "Brocante du dimanche",
+      },
+      {
+        id: "p3",
+        user: "Paul",
+        handle: "@Paul11",
+        img: "images/flux3.png",
+        caption: "Pour les amateurs...",
+      },
+      {
+        id: "p4",
+        user: "752",
+        handle: "@752",
+        img: "images/flux4.png",
+        caption: "Service à thé complet",
+      },
     ],
     following: [
-      { id: "p5", user: "Tom", handle: "@Tom", img: "../images/flux5.png", caption: "Appareil photo vintage" },
-      { id: "p6", user: "Batman", handle: "@Batman", img: "../images/flux6.png", caption: "Vinyles et cadres" },
-      { id: "p7", user: "Thomas", handle: "@Thomas81", img: "../images/flux7.png", caption: "CD rare" },
-      { id: "p4", user: "752", handle: "@752", img: "../images/flux4.png", caption: "Service à thé complet" },
+      {
+        id: "p5",
+        user: "Tom",
+        handle: "@Tom",
+        img: "images/flux5.png",
+        caption: "Appareil photo vintage",
+      },
+      {
+        id: "p6",
+        user: "Batman",
+        handle: "@Batman",
+        img: "images/flux6.png",
+        caption: "Vinyles et cadres",
+      },
+      {
+        id: "p7",
+        user: "Thomas",
+        handle: "@Thomas81",
+        img: "images/flux7.png",
+        caption: "CD rare",
+      },
+      {
+        id: "p4",
+        user: "752",
+        handle: "@752",
+        img: "images/flux4.png",
+        caption: "Service à thé complet",
+      },
     ],
   };
 
